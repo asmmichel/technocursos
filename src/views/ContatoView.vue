fa67e34ce4ab6ecff1ba32a4d610c063d91e5f22
Michel Maciel <michel.asm@hotmail.com>
2023-10-31 14:31:18 -0300

    fechando a logica da pagina contato

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Pagina Contato</h1>
-    <p>{{api}}</p>
+  <div v-if="api" class="conteudo">
+    <h1>{{api.titulo}}</h1>
+    <p>{{api.descricao}}</p>
+    <ul>
+      <li v-for="(value, key) in api.contato" :key="key">
+        <b>{{key.charAt(0).toUpperCase() + key.slice(1)}}:</b> {{value}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -16,3 +21,13 @@ export default {
   }
 }
 </script>
+
+<style>
+  .conteudo {
+    text-align: left;
+    margin: 30px;
+  }
+  li {
+    padding-bottom: 5px;
+  }
+</style>
